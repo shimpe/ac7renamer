ec360196934ce37f0428123dcdbae3ffc52be7ea
stefaan himpe <stefaan.himpe@gmail.com>
2019-10-12 14:00:07 +0200

    add a setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ac7renamer',
+    version='0.0.1',
+    packages=['ac7renamer'],
+    url='https://github.com/shimpe/ac7renamer',
+    license='GPLv3',
+    author='shiihs',
+    author_email='b13d1be5@opayq.com',
+    description='Change display name of AC7 files.'
+)
