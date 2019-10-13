105a87f0a93f22c89cde1871802c6e8596aac11f
stefaan himpe <stefaan.himpe@gmail.com>
2019-10-13 13:30:52 +0200

    fix dependencies

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author='shiihs',
     author_email='b13d1be5@opayq.com',
     description='Change display name of AC7 files.',
-    install_requires=[ "https://github.com/shimpe/ac7parser" ],
+    install_requires=[ "https://github.com/shimpe/ac7parser", "PyQt5" ],
     entry_points={"console_scripts": ["realpython=ac7renamer.ac7renamer:main"]},
 )
