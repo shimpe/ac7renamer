863ea92174f99e09be6346f03353067f14618ad7
stefaan himpe <stefaan.himpe@gmail.com>
2019-10-13 13:30:32 +0200

    fix small layout issue

--- ac7renamer/ac7renamer.py
+++ ac7renamer/ac7renamer.py
@@ -3,6 +3,7 @@ import ac7renamer.myrenamerdlg
 from PyQt5 import QtWidgets
 from pathlib import Path
 
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
     Ac7Ren = QtWidgets.QDialog()
@@ -12,6 +13,7 @@ def main():
     ui.setup_slots(home)
     Ac7Ren.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
