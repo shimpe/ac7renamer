5cdb71e530e5b224b1a0302e106f68bc3d74315c
stefaan himpe <stefaan.himpe@gmail.com>
2021-07-04 10:59:14 +0200

    use a clickable link in the about box instead of a text address

--- ac7renamer/myrenamerdlg.py
+++ ac7renamer/myrenamerdlg.py
@@ -2,7 +2,7 @@ import ac7renamer.ac7renamerdlg
 from PyQt5.QtWidgets import QMessageBox
 from ac7renamer.singlefiletab import SingleFileTab
 from ac7renamer.multifiletab import MultiFileTab
-from PyQt5.QtCore import QSettings
+from PyQt5.QtCore import QSettings, Qt
 from PyQt5.QtGui import QPixmap
 
 
@@ -25,16 +25,17 @@ class MyRenamerDlg(ac7renamer.ac7renamerdlg.Ui_Ac7Renamer):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Information)
         msg.setText("About this tool...")
-        msg.setInformativeText("Tool to change how .AC7 files are displayed by your keyboard.\n\n"
-                               "I strongly encourage you to make a donation to your favorite charity if you decide to keep using this tool.\n\n"
-                               "You can use, modify and copy the tool freely under the guarantees of the GPLv3 license.\n\n"
-                               "Complete source code can be found at https://github.com/shimpe/ac7renamer\n\n"
+        msg.setTextFormat(Qt.RichText)
+        msg.setInformativeText("Tool to manipulate casio .AC7 rhythm files.<br><br>"
+                               "I strongly encourage you to make a donation to your favorite charity if you decide to keep using this tool.<br><br>"
+                               "You can use, modify and copy the tool freely under the guarantees of the GPLv3 license.<br><br>"
+                               "Complete source code can be found at <a href='https://github.com/shimpe/ac7renamer'>GitHub</a><br><br>"
                                "You may copy, distribute and modify the "
                                "software as long as you track changes/dates. Any modifications to, "
                                "or software including (via compiler) GPL-licensed code, must also be made available "
-                               "under the GPL along with build & install instructions.\n\n"
+                               "under the GPL along with build & install instructions.<br><br>"
                                "I try to offer the best possible software, but cannot promise that the software will always be bug-free, available, accurate, complete, and up-to-date. "
-                               "You agree that when you use this software, you do so at your own risk, and this software or affiliated persons and organizations are not in any way responsible for damage or weird behavior inflicted on your instrument. Furthermore, you also agree that you will not attempt to hold us or our data providers liable for damage or inaccuracies in the implementation.\n\n"
+                               "You agree that when you use this software, you do so at your own risk, and this software or affiliated persons and organizations are not in any way responsible for damage or weird behavior inflicted on your instrument. Furthermore, you also agree that you will not attempt to hold us or our data providers liable for damage or inaccuracies in the implementation.<br><br>"
                                "If you cannot agree to these terms, then under no circumstance use this software or files created with this software.")
         msg.setWindowTitle("About")
         msg.setStandardButtons(QMessageBox.Ok)
